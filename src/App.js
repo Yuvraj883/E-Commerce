bbcb1b1932100d821fd351a803f4c7b8de6426e7
Yuvraj <kingyuvi88@gmail.com>
2023-03-07 23:26:23 +0530

    Created Navbar.jsx

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
-  <div className='my-0 bg-black h-12 flex  items-center '>
-    <span className="Bold text-lg italic text-white align-baseline text-justify mx-2 mt-16 inset-y-12">Yuvi's Store
-    <img src="https://www.pngfind.com/pngs/m/453-4531594_icon-05-min-clip-art-shopping-bag-hd.png "
-     className="h-8 w-auto inline mx-2"
-     alt="icon" />
-    </span>
-    <div className="item-end inline ">
-      <img src="https://e7.pngegg.com/pngimages/833/426/png-clipart-shopping-cart-icon-shopping-cart-black-design.png" 
-           className="inline h-8 w-auto "/>
-    </div>
-  </div>
+  <>
+  <Navbar/>
+  </>
   );
 }
 
